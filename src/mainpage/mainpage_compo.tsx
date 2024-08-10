219a9836933485bdc7230b6a4bfdd95a3eb7be90
권도한 <dohan@gwondohans-MacBook-Pro.local>
2024-08-10 20:25:09 +0900

    header

--- src/mainpage/mainpage_compo.tsx
+++ src/mainpage/mainpage_compo.tsx
@@ -4,14 +4,12 @@ export const HeaderBar = () => {
     return (
         <header className={style.header_}>
             <div>
-                <img src="/logo_2.png" style={{width: "90px"}}/>
-                <nav className={style.nav_}>
-                    <span>Home</span>
-                    <span>Home</span>
-                    <span>Home</span>
-                    <span>Home</span>
-                    <span>Home</span>
-                </nav>
+                <img src="/logo_2.png" style={{ width: "90px" }} />
+                <span>Home</span>
+                <span>Home</span>
+                <span>Home</span>
+                <span>Home</span>
+                <span>Home</span>
                 <div className={style.sign_up}>Sign Up</div>
             </div>
         </header>
